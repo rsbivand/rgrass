1764975663c0312258511d725020b827672378c5
Roger Bivand <Roger.Bivand@gmail.com>
2024-09-03 15:42:09 +0200

    curtail test script output

--- tests/test_XML_xml2.R
+++ tests/test_XML_xml2.R
@@ -1,11 +1,13 @@
-library(rgrass)
+suppressPackageStartupMessages(library(rgrass))
+## IGNORE_RDIFF_BEGIN
 old <- readRDS(system.file("etc/XML.r.out.gdal.rds", package = "rgrass"))
 if (nchar(Sys.getenv("GISRC")) > 0) {
   new <- parseGRASS("r.out.gdal")
-  all.equal(old, new)
+  stopifnot(isTRUE(all.equal(old, new)))
 }
 old <- readRDS(system.file("etc/res_r.water.outlet.rds", package = "rgrass"))
 if (nchar(Sys.getenv("GISRC")) > 0) {
   new <- parseGRASS("r.water.outlet")
-  all.equal(old, new)
+  stopifnot(isTRUE(all.equal(old, new)))
 }
+## IGNORE_RDIFF_END
--- tests/test_terra_ptr.R
+++ tests/test_terra_ptr.R
@@ -1,4 +1,4 @@
-library(rgrass)
+suppressPackageStartupMessages(library(rgrass))
 if (requireNamespace("terra", quietly = TRUE)) {
   f <- system.file("ex/elev.tif", package = "terra")
   SG <- terra::rast(f)
